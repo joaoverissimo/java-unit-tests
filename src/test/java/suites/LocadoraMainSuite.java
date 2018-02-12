1f5a74276a48143af850bcde71dd4a277c3abf9c
root <root@verissimo-Inspiron-5457>
2018-02-12 20:17:03 -0200

    Sessão 2 aula 21 - Suite de testes

---
+++ src/test/java/suites/LocadoraMainSuite.java
@@ -0,0 +1,21 @@
+package suites;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+import br.ce.wcaquino.servicos.AssertTest;
+import br.ce.wcaquino.servicos.CalculadoraTest;
+import br.ce.wcaquino.servicos.LocacaoServiceTest;
+import br.ce.wcaquino.servicos.LocacaoValoresTest;
+
+@RunWith(Suite.class)
+@SuiteClasses({
+	CalculadoraTest.class,
+	AssertTest.class,
+	LocacaoServiceTest.class,
+	LocacaoValoresTest.class
+})
+public class LocadoraMainSuite {
+	
+}
